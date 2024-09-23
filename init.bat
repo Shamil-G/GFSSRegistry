654e55bc7bf2c051a5a94996b8ed12d1952ad50c
Shamil-G <s.gusseynov@gmail.com>
2024-09-23 16:44:39 +0500

    COMMIT on SERVER

--- init.bat
+++ init.bat
@@ -2,13 +2,15 @@
 . $VENV_HOME/bin/activate
 
 pip3 install --upgrade pip
-pip3 install oracledb
-pip3 install flask
-pip3 install flask_login
-pip3 install redis
-pip3 install flask_session
-pip3 install openpyxl
-pip3 install requests
-pip3 install gevent
-pip3 install gunicorn
+#pip3 install oracledb
+#pip3 install flask
+#pip3 install flask_login
+#pip3 install redis
+#pip3 install flask_session
+#pip3 install openpyxl
+#pip3 install requests
+#pip3 install gevent
+#pip3 install gunicorn
+pip3 install ldap3
+pip3 install xlsxwriter
 python3 main_app.py
