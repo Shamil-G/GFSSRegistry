8882605261f13fd4bd923d1feb9f5ce9afaec80e
Shamil-G <s.gusseynov@gmail.com>
2025-01-17 18:09:20 +0500

    Добавлен файл model/list_emp.py который можно использовать для выборки всех департаментов или сотрудников одного депртамента Расширен список игнорируемых департаментов

--- model/ldap_function.py
+++ model/ldap_function.py
@@ -1,9 +1,9 @@
-import locale
+# import locale
 from datetime import date, datetime
 from ldap3 import Server, Connection, SUBTREE
-from flask import session
-from app_config import ldap_admins, ldap_server, ldap_user, ldap_password, ldap_ignore_ou, ldap_boss
-from util.ip_addr import ip_addr
+# from flask import session
+from app_config import ldap_server, ldap_user, ldap_password, ldap_ignore_ou#, ldap_admins, ldap_ignore_ou, ldap_boss
+# from util.ip_addr import ip_addr
 from util.logger import log
 
 
@@ -31,6 +31,7 @@ def get_connect(username:str, password:str):
         return ''
     return conn
 
+
 def get_list_birthdate():
     conn_src = get_connect(ldap_user, ldap_password)
 
@@ -56,7 +57,9 @@ def get_list_birthdate():
         src_filter = f'(&(objectclass=person)(| (telephoneNumber=*{curr_month}*) (telephoneNumber=*{next_month}*) ))' 
     conn_src.search(search_base='dc=gfss,dc=kz', 
             # search_filter=f'(&(objectclass=person)(cn=*))', 
-            search_filter=src_filter, 
+
+            search_filter=src_filter,
+
             attributes=['distinguishedName', 'userPrincipalName', 'cn', 'sAMAccountName', 'description', 'memberof', 'telephoneNumber', '*'],
             # attributes=['distinguishedName', 'userPrincipalName', 'cn', 'displayName', 'description', 'memberof', 'telephoneNumber'],
             search_scope=SUBTREE,
@@ -64,6 +67,97 @@ def get_list_birthdate():
     users = conn_src.entries
     # Connection closed
     
+    log.info(f"-------USERS: \n\t{users}\n-------")
+    if len(users)==0:
+        log.debug(f'CONNECT_LDAP. FOUND USERS: {len(users)}\n{users}\n--------------------------------------------')
+        return 0,'',''
+
+    dn=''
+    result_list=[]
+    for user in users:
+        iin = str(user['telephoneNumber'])
+        dn = str(user['distinguishedName'])
+        ou = find_value(dn, 'OU')
+        if ou in ldap_ignore_ou:
+            log.debug(f'UVOLEN! {user}')
+            continue
+        log.debug(f'-------------\nIIN:\n{iin}\n-------------')
+        
+        if (iin[2:4] == curr_month and int(iin[4:6])>=curr_day_int and int(iin[4:6])<curr_day_int+10) or (iin[2:4] == next_month and int(iin[4:6])<10):
+            # cur_locale = locale.getlocale()
+            # locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+            bd_date = datetime.strptime(iin[:6], '%y%m%d')
+            bd_month = int(iin[2:4])
+            if int(curr_month) > bd_month:  # Ситуация в декабре-январе когда 12>1
+                birth_date_sort = f'{int(iin[2:4])+1}.{iin[4:6]}'
+            else:
+                birth_date_sort = f'{iin[2:4]}.{iin[4:6]}'
+            birth_date = bd_date.strftime('%d, %B')
+            result_user = { 
+                            'birth_date': birth_date,
+                            'birth_date_sort': birth_date_sort,
+                            'employee': str(user['displayName']),
+                            'post': str(user['description'])
+                            }
+            log.debug(f'\nDN: {dn}\n-----------------------')
+    
+            conn_src.search(search_base=f'OU={ou},dc=gfss,dc=kz', 
+                        search_filter=f'(objectClass=OrganizationalUnit)', 
+                        # attributes=['name', 'description', '*'],
+                        attributes=['name', 'description'],
+                        search_scope=SUBTREE,
+                        paged_size=1)
+            orgs = conn_src.entries
+
+            log.debug(f'Found organizations: {orgs}')
+            for org in orgs:
+                log.debug(f'---\nORG_UNIT: {ou}, dep_name: {str(org['description'])}\n---')
+            result_user['dep_name'] = str(orgs[0]['description'])
+            result_list.append(result_user)
+    result_list.sort(key=sortElementBD)
+
+    conn_src.unbind()
+
+    log.info(f'GET_LIST_BIRTHDATE. SUCCESS. {result_list}')
+    return result_list
+
+
+def get_list_birthdate_orig():
+    conn_src = get_connect(ldap_user, ldap_password)
+
+    if not conn_src:
+        return 0,'',''
+    
+    next_month=''
+    curr_year = str(date.today())[2:4]
+    curr_centure = str(date.today())[:2]
+    curr_month = str(date.today())[5:7]
+    curr_month_int = int(curr_month)
+    curr_day = str(date.today())[8:10]
+    curr_day_int = int(str(date.today())[8:10])
+    if curr_day_int<25:
+        log.debug(f'GET_LIST_BIRTHDATE. curr_centure: {curr_centure}, curr_year: {curr_year}, curr_month: {curr_month}, curr_day: {curr_day}')
+        src_filter = f'(&(objectclass=person)(| (telephoneNumber=*{curr_month}*) ))' 
+    else:
+        next_month_int = curr_month_int+1
+        if next_month_int>12:
+            next_month_int=1
+        next_month = str(next_month_int).zfill(2)
+        log.debug(f'GET_LIST_BIRTHDATE. curr_centure: {curr_centure}, curr_year: {curr_year}, curr_month: {curr_month}, next_month: {next_month}, curr_day: {curr_day}')
+        src_filter = f'(&(objectclass=person)(| (telephoneNumber=*{curr_month}*) (telephoneNumber=*{next_month}*) ))' 
+    conn_src.search(search_base='dc=gfss,dc=kz', 
+            # search_filter=f'(&(objectclass=person)(cn=*))', 
+
+            search_filter=src_filter,
+
+            attributes=['distinguishedName', 'userPrincipalName', 'cn', 'sAMAccountName', 'description', 'memberof', 'telephoneNumber', '*'],
+            # attributes=['distinguishedName', 'userPrincipalName', 'cn', 'displayName', 'description', 'memberof', 'telephoneNumber'],
+            search_scope=SUBTREE,
+            paged_size=250)
+    users = conn_src.entries
+    # Connection closed
+    
+    log.info(f"-------USERS: \n\t{users}\n-------")
     if len(users)==0:
         log.debug(f'CONNECT_LDAP. FOUND USERS: {len(users)}\n{users}\n--------------------------------------------')
         return 0,'',''
@@ -84,7 +178,7 @@ def get_list_birthdate():
             # locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
             bd_date = datetime.strptime(iin[:6], '%y%m%d')
             bd_month = int(iin[2:4])
-            if int(curr_month) > bd_month:  # �������� � �������-������ ����� 12>1
+            if int(curr_month) > bd_month:  # Ситуация в декабре-январе когда 12>1
                 birth_date_sort = f'{int(iin[2:4])+1}.{iin[4:6]}'
             else:
                 birth_date_sort = f'{iin[2:4]}.{iin[4:6]}'
