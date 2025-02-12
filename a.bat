b3902c78ec57fa5be4c38b46bb5017be9cc5a6d2
Shamil-G <s.gusseynov@gmail.com>
2025-02-12 12:21:20 +0500

    Упрощен стиль кнопок в папке styles/issatay

---
+++ a.bat
@@ -0,0 +1,6 @@
+git config --global user.name "Shamil-G"
+git config --global user.email s.gusseynov@gmail.com
+
+git remote add origin https://github.com/Shamil-G/GFSSRegistry.git
+git branch -M main
+git push -u origin main
