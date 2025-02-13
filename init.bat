e6d6d80ea0b7659433783ddafe0d1f7ccbaad60b
Shamil-G <s.gusseynov@gmail.com>
2025-02-13 11:28:20 +0500

    Изменены стили некоторых элементов

--- init.bat
+++ init.bat
@@ -1,16 +1,18 @@
-#python3 -m venv venv
-. $VENV_HOME/bin/activate
+rem python -m venv venv
+rem . /home/registry/venv/bin/activate
 
-pip3 install --upgrade pip
-#pip3 install oracledb
-#pip3 install flask
-#pip3 install flask_login
-#pip3 install redis
-#pip3 install flask_session
-#pip3 install openpyxl
-#pip3 install requests
-#pip3 install gevent
-#pip3 install gunicorn
+call C:\Projects\GFSSRegistry\venv\Scripts\activate.bat
+
+python -m pip install --upgrade pip
+pip3 install oracledb
+pip3 install flask
+pip3 install flask_login
+pip3 install redis
+pip3 install flask_session
+pip3 install openpyxl
+pip3 install requests
+pip3 install gevent
+pip3 install gunicorn
 pip3 install ldap3
 pip3 install xlsxwriter
-python3 main_app.py
+python main_app.py
